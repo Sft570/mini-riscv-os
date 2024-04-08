3ce4438e1ddeca993cdf901803a068b4c9ca6d82
Sft570 <sft570@alumnos.unican.es>
2024-04-08 19:53:21 +0200

    ejemplo practico asm

--- 01-HelloOs/os.c
+++ 01-HelloOs/os.c
@@ -14,9 +14,17 @@ void lib_puts(char *s) {
 	while (*s) lib_putc(*s++);
 }
 
+int programa_alumno() {
+    int a = 10;
+    int b = 20;
+    int resultado = suma_asm(a, b);
+    return 0;
+}
+
 int os_main(void)
 {
 	lib_puts("Hello OS!\n");
+	programa_alumno();
 	while (1) {}
 	return 0;
 }
